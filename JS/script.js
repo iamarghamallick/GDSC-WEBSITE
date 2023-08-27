2c466db76e12fda231aad78d8833f90e91a0bc18
Argha Mallick <arghamallick5@gmail.com>
2023-08-27 21:57:11 +0530

    content marged

--- JS/script.js
+++ JS/script.js
@@ -59,3 +59,30 @@ function printStringByLetter(paragraph_id, myDiv_id) {
 function clearBox(elementID) {
     document.getElementById(elementID).innerHTML = "";
 }
+
+
+
+let accordianHead = Array.from(document.querySelectorAll(".accordian_head"));
+
+accordianHead.map((item) =>
+    item.addEventListener("click", () => {
+        closeAllAccordian(item);
+    })
+);
+
+function closeAllAccordian(current_target) {
+    console.log(current_target);
+    accordianHead.map((item) => {
+        if (current_target !== item) {
+            const accordianBody = item.nextElementSibling;
+            const togglerBtn = item.firstElementChild;
+            togglerBtn.classList.remove("active");
+            accordianBody.classList.remove("active_body");
+        } else {
+            const accordianBody = current_target.nextElementSibling;
+            const togglerBtn = item.firstElementChild;
+            togglerBtn.classList.toggle("active");
+            accordianBody.classList.toggle("active_body");
+        }
+    });
+}
