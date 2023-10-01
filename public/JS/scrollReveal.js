c666cefbba48d904f7df1094d407eb551027b1b6
Argha Mallick <arghamallick5@gmail.com>
2023-10-01 21:07:46 +0530

    texture and scroll revel added

---
+++ public/JS/scrollReveal.js
@@ -0,0 +1,63 @@
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'left'
+    }
+).reveal('.about-left');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'right'
+    }
+).reveal('.about-right');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'bottom'
+    }
+).reveal('.domain-card');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'bottom'
+    }
+).reveal('.gallery-container');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'bottom'
+    }
+).reveal('.accordian');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'bottom'
+    }
+).reveal('.sponsor');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'left'
+    }
+).reveal('.contact-links');
+
+ScrollReveal(
+    {
+        distance: '60px',
+        delay: '300',
+        origin: 'right'
+    }
+).reveal('.contact-form-wrapper');
